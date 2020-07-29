452f31c2d2b5b5e70ace1dfdbc653d4f78d5f9e3
Michelle <michelle_broomes@yahoo.com>
2020-07-29 00:13:55 -0400

    fixing nsticky av baar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,23 +4,25 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-window.onscroll = function() {stickyNav()};
+window.onscroll = function() {
+  stickyNav()
+}
 
 function stickyNav() {
-  if (window.pageYOffset >= 55) {
-    navbar.classList.add("sticky")
+  if (window.pageYOffset >= 100) {
+    navbar.classList.add('sticky')
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove('sticky')
   }
 }
 
 const onClick = () => {
-  mySidenav.style.width = "250px"
+  mySidenav.style.width = '250px'
 }
 
-const closeNav = (e) => {
+const closeNav = e => {
   e.preventDefault()
-  mySidenav.style.width = "0"
+  mySidenav.style.width = '0'
 }
 
 const Navbar = ({handleClick, isLoggedIn, isAdmin}) => (
@@ -30,88 +32,98 @@ const Navbar = ({handleClick, isLoggedIn, isAdmin}) => (
         <div id="navBar-container">
           {/* The navbar will show these links after you log in */}
           <div className="nav-items">
-          <div className="left-nav">
-          <Link to="/home">Home</Link>
-          <div className="dropdown">
-          <Link to="/products" className="dropbtn">All Products <i className="fa fa-caret-down"></i></Link>
-            <div className="dropdown-content">
-            <Link to="/products/cookies">Cookies</Link>
-            <Link to="/products/chocolates">Chocolates</Link>
-            <Link to="/products/cakepops">Cakepops</Link>
-            <Link to="/products/cupcakes">Cupcakes</Link>
+            <div className="left-nav">
+              <Link to="/home">Home</Link>
+              <div className="dropdown">
+                <Link to="/products" className="dropbtn">
+                  All Products <i className="fa fa-caret-down" />
+                </Link>
+                <div className="dropdown-content">
+                  <Link to="/products/cookies">Cookies</Link>
+                  <Link to="/products/chocolates">Chocolates</Link>
+                  <Link to="/products/cakepops">Cakepops</Link>
+                  <Link to="/products/cupcakes">Cupcakes</Link>
+                </div>
+              </div>
             </div>
-          </div>
-          </div>
-          <div id="mySidenav" className="mySidenav">
-            <a href="" className="closebtn" onClick={closeNav}>&times;</a>
+            <div id="mySidenav" className="mySidenav">
+              <a href="" className="closebtn" onClick={closeNav}>
+                &times;
+              </a>
               <Link to="/home">Home</Link>
               <Link to="/products">All Sweets</Link>
               <div className="extra-sides">
-              <Link to="/products/cookies">Cookies</Link>
-              <Link to="/products/chocolates">Chocolates</Link>
-              <Link to="/products/cakepops">Cakepops</Link>
-              <Link to="/products/cupcakes">Cupcakes</Link>
+                <Link to="/products/cookies">Cookies</Link>
+                <Link to="/products/chocolates">Chocolates</Link>
+                <Link to="/products/cakepops">Cakepops</Link>
+                <Link to="/products/cupcakes">Cupcakes</Link>
               </div>
               {isAdmin ? (
                 <Link to="/users">Users</Link>
-                ) : (
+              ) : (
                 <Link to="/cart">Cart</Link>
               )}
               <a href="#" onClick={handleClick}>
                 Logout
               </a>
-          </div>
-          <a className='nav-icon' onClick={onClick}>
-              <span></span><span></span><span></span>
-          </a>
-          <div className="mid-nav">
-          <h1>Coocucachew</h1>
-          </div>
-          <div className="right-nav">
-          {isAdmin ? (
-            <Link to="/users">Users</Link>
-          ) : (
-            <Link to="/cart">Cart</Link>
-          )}
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-          </div>
+            </div>
+            <a className="nav-icon" onClick={onClick}>
+              <span />
+              <span />
+              <span />
+            </a>
+            <div className="mid-nav">
+              <h1>Coocucachew</h1>
+            </div>
+            <div className="right-nav">
+              {isAdmin ? (
+                <Link to="/users">Users</Link>
+              ) : (
+                <Link to="/cart">Cart</Link>
+              )}
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </div>
           </div>
         </div>
       ) : (
         <div className="nav-items">
           {/* The navbar will show these links before you log in */}
           <div className="left-nav">
-          <Link to="/products">Home</Link>
+            <Link to="/products">Home</Link>
           </div>
           <div id="mySidenav" className="mySidenav">
-            <a href="" className="closebtn" onClick={closeNav}>&times;</a>
-              <Link to="/products">All Sweets</Link>
-              <div className="extra-sides">
+            <a href="" className="closebtn" onClick={closeNav}>
+              &times;
+            </a>
+            <Link to="/products">All Sweets</Link>
+            <div className="extra-sides">
               <Link to="/products/cookies">Cookies</Link>
               <Link to="/products/chocolates">Chocolates</Link>
               <Link to="/products/cakepops">Cakepops</Link>
               <Link to="/products/cupcakes">Cupcakes</Link>
-              </div>
-              {isAdmin ? (
-                <Link to="/users">Users</Link>
-                ) : (
-                <Link to="/cart">Cart</Link>
-              )}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
+            </div>
+            {isAdmin ? (
+              <Link to="/users">Users</Link>
+            ) : (
+              <Link to="/cart">Cart</Link>
+            )}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
           </div>
-          <a className='nav-icon' onClick={onClick}>
-              <span></span><span></span><span></span>
+          <a className="nav-icon" onClick={onClick}>
+            <span />
+            <span />
+            <span />
           </a>
           <div className="mid-nav">
-          <h1>Coocucachew</h1>
+            <h1>Coocucachew</h1>
           </div>
           <div className="right-nav">
-          <Link to="/cart">Cart</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+            <Link to="/cart">Cart</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
           </div>
         </div>
       )}
